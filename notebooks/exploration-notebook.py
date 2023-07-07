cb8b5ae4f45ad47575c858bf8c8810c0cc34becd
Motin <motin@motin.eu>
2023-07-07 08:45:08 +0300

    Add Jupyter and token monitoring deps and a exploration notebook placeholder (#6)
    This update involves reordering dependencies in the pyproject.toml file
    and includes addition of 'langchain', 'openai', 'gpt4all', 'tokmon', and
    'mitmproxy'. A new 'poe' command has been introduced for installing a
    kernel and starting Jupyter. An exploration notebook placeholder has
    also been added to the project.

---
+++ notebooks/exploration-notebook.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.6
+#   kernelspec:
+#     display_name: autonomous-engineer
+#     language: python
+#     name: autonomous-engineer
+# ---
+
+# Makes edits in the included files available without restarting the kernel
+# %reload_ext autoreload
+# %autoreload 2
+

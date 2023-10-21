ae51e9a897179be9bbbd4759be7afe4786796962
Motin <motin@motin.eu>
2023-10-21 12:29:26 +0300

    Auto-formatting

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -1,8 +1,8 @@
-import discord
 from discord.ext import commands
 from pydantic_settings import BaseSettings
 
 from discord import Intents
+
 
 class Config(BaseSettings):
     TOKEN: str
@@ -10,20 +10,24 @@ class Config(BaseSettings):
     class Config:
         env_file = ".env"
 
+
 config = Config()
 
 intents = Intents.default()
 intents.message_content = True
 intents.messages = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f"We have logged in as {bot.user}")
+
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send('Hello!')
+    await ctx.send("Hello!")
+
 
 bot.run(config.TOKEN)
